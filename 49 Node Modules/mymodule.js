06f8b45568b1ebb8613518be75470e062ff81458
AryanVerma72 <aryankumar827398@gmail.com>
2026-08-17 01:18:43 +0530

    change in mymodule

--- 49 Node Modules/mymodule.js
+++ 49 Node Modules/mymodule.js
@@ -1,8 +1,8 @@
-// export const a = 1
-// export const b = 2
-// export const c = 3
-// export const d = 4
-// export const e = 5
+export const a = 1
+export const b = 2
+export const c = 3
+export const d = 4
+export const e = 5
 
 let obj;
 export default obj = {

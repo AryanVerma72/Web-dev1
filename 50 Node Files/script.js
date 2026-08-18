892c8af4088aa7f5716ad888fc7a32eeda7b03ca
AryanVerma72 <aryankumar827398@gmail.com>
2026-08-18 23:40:36 +0530

    3rd tute

--- 50 Node Files/script.js
+++ 50 Node Files/script.js
@@ -1,1 +1,20 @@
 console.log("Working With  files")
+const fs = require("fs")
+console.log(fs)
+
+
+// fs.writeFileSync("Aryan.txt", "My number is 923849817")
+console.log("Starting")
+
+fs.writeFile("Aryan 2.txt", "My age is 21",()=>{
+    console.log("Done")
+    fs.readFile("Aryan 2.txt", (error, data)=>{
+        console.log(error,data.toString())
+    })
+})
+
+fs.appendFile("Aryan.txt" , "AryanMagic", (e,d)=>{
+    console.log(d)
+})
+
+console.log("Ending")

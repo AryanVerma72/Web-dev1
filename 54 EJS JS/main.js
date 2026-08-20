911a4eecab84ef929317c1884374e2dcf5ba5cab
AryanVerma72 <aryankumar827398@gmail.com>
2026-08-20 23:49:40 +0530

    EJS

--- 54 EJS JS/main.js
+++ 54 EJS JS/main.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express()
 const port = 3000
 
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.sendFile("templates/index.html" , {root : __dirname})
+    let siteName = "Adidas"
+    let searchText = "Search now"
+    let arr = [3,23,52]
+  res.render("index" , {siteName : siteName , searchText : searchText , arr: arr
+  })
+})
+
+app.get('/blog/:slug', (req, res) => {
+    let blogTitle = "Adidas when and why ?"
+    let blogContent = "It is very Good Brand"
+  res.render("blogPost" , {blogTitle : blogTitle, blogContent : blogContent})
 })
 
 app.listen(port, () => {
